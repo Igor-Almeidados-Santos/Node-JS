1d22961f89cf1e2bb6f7fc8b727238ce227f3c9a
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-04-16 15:20:56 -0300

    Código NodeJs

--- modulos/Padrao-Chain-of-responsability/middleware.js
+++ modulos/Padrao-Chain-of-responsability/middleware.js
@@ -1,5 +1,7 @@
 //O padrão chain of responsability ou middleware é um padrão que permite o reuso de funções separando os passos e conectando eles através de uma outra função que dispara a proxima necessaria no ciclo.
 
+
+//Conjunto de passos que a função pode ou não utilizar.
 const passo1 = (ctx, next) =>{
     ctx.valor1 ='mid1';
     next();
@@ -11,3 +13,20 @@ const passo2 = (ctx, next) =>{
 };
 
 const passo3 = ctx => ctx.valor3 ='mid3';
+
+
+//Função que fica encarregada de executar os passos na ordem informada.
+const exec = (ctx, ...middlewares) => {//Middlewares é um array com as funções/passos que serão executadas.
+    const execPasso = indice => {
+        middlewares && indice < middlewares.length && middlewares[indice](ctx, () => execPasso (indice + 1));//Essa função execulta os passos em ordem, ela verifica primeiro se middlewares está setado, depois verifica se o indice passado é menor que o tamanho de middleware e por ultimo execulta a função que está na posição do indice informado passando como função next o proprio execpasso novamente agora com indice +1.
+    };
+
+    execPasso(0);
+};
+
+//Execussão dos passos
+const ctx = {};
+
+exec(ctx, passo1, passo2);//Executa cada passo na ordem informada.
+
+console.log(ctx);
